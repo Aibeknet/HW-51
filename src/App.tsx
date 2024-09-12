8ef1170465cb80daec33eded4c4b3c4ebadddfc7
aibeknet <aaskarov0690.kg@gmail.com>
2024-09-12 16:34:19 +0500

    Created main logic for generating and show numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import React, { useState } from 'react';
+import Showball from './components/Showball';
 import './App.css'
 
-const App = () => {
+const initialNumbers = [5, 11, 16, 23, 32];
 
-  return (
-    <>
-      Hello world!
-    </>
-  )
+const App: React.FC = () => {
+    const [numbers, setNumbers] = useState<number[]>(initialNumbers);
+
+    const randomNumbers = () => {
+        const newNumbers: number[] = [];
+        while (newNumbers.length < 5) {
+            const num = Math.floor(Math.random() * 36) + 5;
+            if (!newNumbers.includes(num)) {
+                newNumbers.push(num);
+            }
+        }
+        newNumbers.sort((a, b) => a - b);
+        setNumbers(newNumbers);
+    };
+    return (
+        <div className="app">
+            <h1>Lottery 5 из 36</h1>
+            <div className="balls">
+                {numbers.map((number, index) => (
+                    <Showball key={index} number={number}/>
+                ))}
+            </div>
+            <button onClick={randomNumbers}>New numbers</button>
+        </div>
+    );
 };
 
 export default App
